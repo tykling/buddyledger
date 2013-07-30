dbe1179868fd3ae2e2eadb9f6b44ac2271f2e80d
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-07-30 22:32:52 +0200

    working on edit ledger

--- src/buddyledger/views.py
+++ src/buddyledger/views.py
@@ -31,6 +31,14 @@ def EditLedger(request, ledgerid):
         response = render_to_response('ledgerdoesnotexist.html')
         return response
 
+    if request.method == 'POST': # If the form has been submitted...
+        form = LedgerForm(instance=ledger) # A form bound to the ledger data
+        if form.is_valid(): # All validation rules pass
+            ledger = form.save() # save the new ledger
+            return HttpResponseRedirect('/ledger/%s' % ledger.id) # return to the ledger page
+    else:
+        form = LedgerForm(instance=ledger)
+
     return render(request, 'editledger.html', {
-        'ledgerid': ledgerid,
+        'form': form,
     })
