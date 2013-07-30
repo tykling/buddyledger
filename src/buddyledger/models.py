9ff86741e3773b5731c7270aaa3c3856b12f342f
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-07-30 22:59:04 +0200

    max_length=100 on CharFields

--- src/buddyledger/models.py
+++ src/buddyledger/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Ledger(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
 
 class Person(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     ledger = models.ForeignKey(Ledger)
 
 class Expense(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     amount = models.BigIntegerField()
 
 class ExpensePerson(models.Model):
