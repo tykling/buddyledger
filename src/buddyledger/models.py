a6b94fa8c372bee46a8030d6d1aca844ae644e09
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-07-30 22:57:08 +0200

    change textfield to CharField

--- src/buddyledger/models.py
+++ src/buddyledger/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Ledger(models.Model):
-    name = models.TextField()
+    name = models.CharField()
 
 class Person(models.Model):
-    name = models.TextField()
+    name = models.CharField()
     ledger = models.ForeignKey(Ledger)
 
 class Expense(models.Model):
-    name = models.TextField()
+    name = models.CharField()
     amount = models.BigIntegerField()
 
 class ExpensePerson(models.Model):
