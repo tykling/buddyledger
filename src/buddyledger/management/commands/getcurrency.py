4e98fdba15969ce009edc27e4c80a816adce64c9
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-07-31 18:46:51 +0200

    working on currency download

--- src/buddyledger/management/commands/getcurrency.py
+++ src/buddyledger/management/commands/getcurrency.py
@@ -1,12 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
 from buddyledger.models import Currency
 import urllib
+import xml.etree.ElementTree as etree
 
 class Command(BaseCommand):
     help = 'Gets currency exchange rates from http://nationalbanken.dk/dndk/valuta.nsf/valuta.xml'
 
     def handle(self, *args, **options):
-        tree = etree.parse('http://nationalbanken.dk/dndk/valuta.nsf/valuta.xml')
+        f = urllib.urlopen('http://nationalbanken.dk/dndk/valuta.nsf/valuta.xml')
+        xml = f.read()
+        f.close()
+        tree = etree.parse(xml)
         root = tree.getroot()
         for child in root[0]:
             try:
@@ -16,4 +20,4 @@ class Command(BaseCommand):
                 currency = Currency(code=child.attrib['code'],danish_ore_price=child.attrib['rate']/100)
             
             currency.save()
-            self.stdout.write('Saved rate: 100 %s costs %s danish ore' % (child.attrib['code'],child.attrib['rate']/100)
+            self.stdout.write('Saved rate: 100 %s costs %s danish ore' % (child.attrib['code'],child.attrib['rate']/100))
