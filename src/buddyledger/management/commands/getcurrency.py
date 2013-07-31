711c1739ef1fd90d011f83353e14cd9be377f221
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-07-31 18:53:32 +0200

    working on currency download

--- src/buddyledger/management/commands/getcurrency.py
+++ src/buddyledger/management/commands/getcurrency.py
@@ -10,14 +10,15 @@ class Command(BaseCommand):
         f = urllib.urlopen('http://nationalbanken.dk/dndk/valuta.nsf/valuta.xml')
         xml = f.read()
         f.close()
-        tree = etree.parse(xml)
-        root = tree.getroot()
-        for child in root[0]:
+        tree = etree.fromstring(xml)
+        for child in tree[0]:
             try:
                 currency = Currency.objects.get(iso4217_code=child.attrib['code'])
                 currency.danish_ore_price=child.attrib['rate']/100
+                temp = ""
             except Currency.DoesNotExist:
                 currency = Currency(code=child.attrib['code'],danish_ore_price=child.attrib['rate']/100)
+                temp = " new"
             
             currency.save()
-            self.stdout.write('Saved rate: 100 %s costs %s danish ore' % (child.attrib['code'],child.attrib['rate']/100))
+            self.stdout.write('Saved%s rate: 100 %s costs %s danish ore' % (temp, child.attrib['code'],child.attrib['rate']/100))
