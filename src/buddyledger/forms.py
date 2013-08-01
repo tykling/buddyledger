eaf5df52585f5232583bbe2bff5ab443c517e9a3
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-08-01 18:42:12 +0200

    permit decimals

--- src/buddyledger/forms.py
+++ src/buddyledger/forms.py
@@ -1,16 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from buddyledger.models import Ledger
 from buddyledger.models import Person
 from buddyledger.models import Expense
 from buddyledger.models import Payment
 
+
 class LedgerForm(ModelForm):
     class Meta:
         model = Ledger
 
+
 class PersonForm(ModelForm):
     class Meta:
         model = Person
+
 
 class ExpenseForm(ModelForm):
     class Meta:
@@ -20,7 +23,7 @@ class ExpenseForm(ModelForm):
     ### since amount is a bigint field the default validation rules do not allow decimals,
     ### just check that amount is numeric and send it on
     def clean_amount(self):
-        data = self.cleaned_data['amount']
+        data = self.cleaned_data.get('amount')
         try:
             float(data)
             return data
@@ -36,7 +39,7 @@ class PaymentForm(ModelForm):
     ### since amount is a bigint field the default validation rules do not allow decimals,
     ### just check that amount is numeric and send it on
     def clean_amount(self):
-        data = self.cleaned_data['amount']
+        data = self.cleaned_data.get('amount')
         try:
             float(data)
             return data
