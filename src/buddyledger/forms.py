1a057ed9cd476eff1a7801bb941f150c5e1538f1
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-08-01 18:58:23 +0200

    use decimalfield instead of bigint for amounts

--- src/buddyledger/forms.py
+++ src/buddyledger/forms.py
@@ -19,29 +19,7 @@ class ExpenseForm(ModelForm):
     class Meta:
         model = Expense
 
-    ### custom validation function for the amount field,
-    ### since amount is a bigint field the default validation rules do not allow decimals,
-    ### just check that amount is numeric and send it on
-    def clean_amount(self):
-        data = self.cleaned_data.get('amount')
-        try:
-            float(data)
-            return data
-        except ValueError:
-            raise forms.ValidationError("invalid amount")
-
 
 class PaymentForm(ModelForm):
     class Meta:
         model = Payment
-
-    ### custom validation function for the amount field,
-    ### since amount is a bigint field the default validation rules do not allow decimals,
-    ### just check that amount is numeric and send it on
-    def clean_amount(self):
-        data = self.cleaned_data.get('amount')
-        try:
-            float(data)
-            return data
-        except ValueError:
-            raise forms.ValidationError("invalid amount")
--- src/buddyledger/models.py
+++ src/buddyledger/models.py
@@ -17,7 +17,7 @@ class Person(models.Model):
 
 class Expense(models.Model):
     name = models.CharField(max_length=100)
-    amount = models.BigIntegerField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
     currency = models.ForeignKey('Currency')
     ledger = models.ForeignKey('Ledger',editable=False)
     people = models.ManyToManyField('Person',null=True)
@@ -29,13 +29,13 @@ class Expense(models.Model):
 class Payment(models.Model):
     expense = models.ForeignKey('Expense',editable=False)
     person = models.ForeignKey('Person')
-    amount = models.BigIntegerField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
     currency = models.ForeignKey('Currency',editable=False)
 
 
 class Currency(models.Model):
     iso4217_code = models.CharField(max_length=3)
-    danish_ore_price = models.BigIntegerField()
+    dkk_price_for_100 = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __unicode__(self):
         return self.iso4217_code
