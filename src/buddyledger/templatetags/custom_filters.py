06ff2e515b76b30e93077442e7c6f545e1c10781
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-08-07 18:26:41 +0200

    more work on output

--- src/buddyledger/templatetags/custom_filters.py
+++ src/buddyledger/templatetags/custom_filters.py
@@ -1,13 +1,15 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-
+from decimal import *
 register = template.Library()
 
 
 @register.filter
-def keyvalue(dict, key):    
+def keyvalue(dict, key):
     return dict[key]
-    
+
 @register.filter
 def templateabs(value):
-    return abs(value)
+    if value != "n/a":
+        value = abs(Decimal(value))
+    return value
