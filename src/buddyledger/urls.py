15877fb2a848251d4bea996203a1c1aa6486ad78
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-09-25 19:02:47 +0200

    working on imports

--- src/buddyledger/urls.py
+++ src/buddyledger/urls.py
@@ -1,37 +1,30 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     ### frontpage and other pages
-    url(r'^$', 'buddyledger.views.Frontpage'),
-    url(r'^usage/$', 'buddyledger.views.ShowUsage'),
+    url(r'^$', 'buddyledger.presentation.Frontpage'),
+    url(r'^usage/$', 'buddyledger.presentation.ShowUsage'),
     
     ### ledger
-    url(r'^ledger/create/$', 'buddyledger.views.CreateLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/$', 'buddyledger.views.ShowLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/edit/$', 'buddyledger.views.EditLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/close/$', 'buddyledger.views.CloseLedger'),
+    url(r'^ledger/create/$', 'buddyledger.ledger.CreateLedger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/$', 'buddyledger.ledger.ShowLedger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/edit/$', 'buddyledger.ledger.EditLedger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/close/$', 'buddyledger.ledger.CloseLedger'),
 
     ### person
-    url(r'^ledger/(?P<ledgerid>\d+)/addperson/$', 'buddyledger.views.AddPerson'),
-    url(r'^person/(?P<personid>\d+)/edit/$', 'buddyledger.views.EditPerson'),
-    url(r'^person/(?P<personid>\d+)/remove/$', 'buddyledger.views.RemovePerson'),
+    url(r'^ledger/(?P<ledgerid>\d+)/addperson/$', 'buddyledger.person.AddPerson'),
+    url(r'^person/(?P<personid>\d+)/edit/$', 'buddyledger.person.EditPerson'),
+    url(r'^person/(?P<personid>\d+)/remove/$', 'buddyledger.person.RemovePerson'),
     
     ### expense
-    url(r'^ledger/(?P<ledgerid>\d+)/addexpense/$', 'buddyledger.views.AddExpense'),
-    url(r'^expense/(?P<expenseid>\d+)/edit/$', 'buddyledger.views.EditExpense'),
-    url(r'^expense/(?P<expenseid>\d+)/remove/$', 'buddyledger.views.RemoveExpense'),
-    url(r'^expense/(?P<expenseid>\d+)/addperson/(?P<personid>\d+)/$', 'buddyledger.views.ExpenseAddPerson'),
-    url(r'^expense/(?P<expenseid>\d+)/removeperson/(?P<personid>\d+)/$', 'buddyledger.views.ExpenseRemovePerson'),
+    url(r'^ledger/(?P<ledgerid>\d+)/addexpense/$', 'buddyledger.expense.AddExpense'),
+    url(r'^expense/(?P<expenseid>\d+)/edit/$', 'buddyledger.expense.EditExpense'),
+    url(r'^expense/(?P<expenseid>\d+)/remove/$', 'buddyledger.expense.RemoveExpense'),
+    url(r'^expense/(?P<expenseid>\d+)/addperson/(?P<personid>\d+)/$', 'buddyledger.expense.ExpenseAddPerson'),
+    url(r'^expense/(?P<expenseid>\d+)/removeperson/(?P<personid>\d+)/$', 'buddyledger.expense.ExpenseRemovePerson'),
 
     ### payments
-    url(r'^expense/(?P<expenseid>\d+)/addpayment/$', 'buddyledger.views.AddPayment'),
-    url(r'^payment/(?P<paymentid>\d+)/edit/$', 'buddyledger.views.EditPayment'),
-    url(r'^payment/(?P<paymentid>\d+)/remove/$', 'buddyledger.views.RemovePayment'),
-    
-    # Admin urls
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^expense/(?P<expenseid>\d+)/addpayment/$', 'buddyledger.payment.AddPayment'),
+    url(r'^payment/(?P<paymentid>\d+)/edit/$', 'buddyledger.payment.EditPayment'),
+    url(r'^payment/(?P<paymentid>\d+)/remove/$', 'buddyledger.payment.RemovePayment'),
 )
