d3aa366d06a86bd3a1e2361bd1b2c262cf93ec87
Thomas Steen Rasmussen <thomas@gibfest.dk>
2015-08-12 22:25:18 +0200

    add names for urls

--- src/buddyledger/urls.py
+++ src/buddyledger/urls.py
@@ -2,26 +2,26 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     ### frontpage and other static pages
-    url(r'^$', 'buddyledger.views.Frontpage'),
-    url(r'^usage/$', 'buddyledger.views.ShowUsage'),
+    url(r'^$', 'buddyledger.views.Frontpage', name='frontpage'),
+    url(r'^usage/$', 'buddyledger.views.ShowUsage', name='show_usage'),
     
     ### ledger
-    url(r'^ledger/create/$', 'buddyledger.views.CreateLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/$', 'buddyledger.views.ShowLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/edit/$', 'buddyledger.views.EditLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/close/$', 'buddyledger.views.CloseLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/reopen/$', 'buddyledger.views.ReopenLedger'),
-    url(r'^ledger/(?P<ledgerid>\d+)/changemethod/$', 'buddyledger.views.ChangeMethod'),
+    url(r'^ledger/create/$', 'buddyledger.views.CreateLedger', name='create_ledger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/$', 'buddyledger.views.ShowLedger', name='show_ledger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/edit/$', 'buddyledger.views.EditLedger', name='edit_ledger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/close/$', 'buddyledger.views.CloseLedger', name='close_ledger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/reopen/$', 'buddyledger.views.ReopenLedger', name='reopen_ledger'),
+    url(r'^ledger/(?P<ledgerid>\d+)/changemethod/$', 'buddyledger.views.ChangeMethod', name='change_ledger_method'),
 
     ### person
-    url(r'^ledger/(?P<ledgerid>\d+)/addperson/$', 'buddyledger.views.AddPerson'),
-    url(r'^person/(?P<personid>\d+)/edit/$', 'buddyledger.views.EditPerson'),
-    url(r'^person/(?P<personid>\d+)/remove/$', 'buddyledger.views.RemovePerson'),
+    url(r'^ledger/(?P<ledgerid>\d+)/addperson/$', 'buddyledger.views.AddPerson', name='add_person'),
+    url(r'^person/(?P<personid>\d+)/edit/$', 'buddyledger.views.EditPerson', name='edit_person'),
+    url(r'^person/(?P<personid>\d+)/remove/$', 'buddyledger.views.RemovePerson', name='remove_person'),
     
     ### expense
-    url(r'^ledger/(?P<ledgerid>\d+)/addexpense/$', 'buddyledger.views.AddExpense'),
-    url(r'^expense/(?P<expenseid>\d+)/edit/$', 'buddyledger.views.EditExpense'),
-    url(r'^expense/(?P<expenseid>\d+)/remove/$', 'buddyledger.views.RemoveExpense'),
-    url(r'^expense/(?P<expenseid>\d+)/addperson/(?P<personid>\d+)/$', 'buddyledger.views.ExpenseAddPerson'),
-    url(r'^expense/(?P<expenseid>\d+)/removeperson/(?P<personid>\d+)/$', 'buddyledger.views.ExpenseRemovePerson'),
+    url(r'^ledger/(?P<ledgerid>\d+)/addexpense/$', 'buddyledger.views.AddExpense', name='add_expense'),
+    url(r'^expense/(?P<expenseid>\d+)/edit/$', 'buddyledger.views.EditExpense', name='edit_expense'),
+    url(r'^expense/(?P<expenseid>\d+)/remove/$', 'buddyledger.views.RemoveExpense', name='remove_expense'),
+    url(r'^expense/(?P<expenseid>\d+)/addperson/(?P<personid>\d+)/$', 'buddyledger.views.ExpenseAddPerson', name='expense_add_person'),
+    url(r'^expense/(?P<expenseid>\d+)/removeperson/(?P<personid>\d+)/$', 'buddyledger.views.ExpenseRemovePerson', name='expense_remove_person'),
 )
