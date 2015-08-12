815aabf8b79c6831cd837d0a8b561c44338d5513
Janus <ysangkok@gmail.com>
2015-08-12 23:26:16 +0200

    do not delete used people

--- src/buddyledger/views/graphbuilder.py
+++ src/buddyledger/views/graphbuilder.py
@@ -5,11 +5,20 @@ from networkx import nx
 from fractions import Fraction
 from collections import defaultdict
 
-def solve_mincost_problem_for_expenses(expenses, people):
+def force_feasible(expenses, people):
+    """ this adds an additional person that is involved in all expenses, which will often (if not always) make the problem feasible """
+    newpeople = list(itertools.islice(filter(lambda x: x not in people, itertools.count(88888888)), 1))
+    def fix(a):
+        a.update({newpeople[0]: None})
+        return a
+    return wrapped([{"whopaid": expense["whopaid"], "whoshouldpay": fix(expense["whoshouldpay"])} for expense in expenses], newpeople + people)
+
+def solve_mincost_problem_for_expenses(expenses, people, debug=False):
+    if debug: print(expenses)
     nodes = defaultdict(lambda: 0)
     
     G = nx.DiGraph()
-    for expense in expenses:
+    for idx, expense in enumerate(expenses):
     
         whopaid = expense["whopaid"]
         whoshouldpay = expense["whoshouldpay"]
@@ -39,6 +48,11 @@ def solve_mincost_problem_for_expenses(expenses, people):
     return flowDict
 
 if __name__ == "__main__":
-    expenses = [{"whopaid": [{"personId": 10, "amount": Fraction(1,2)}, {"personId": 11, "amount": Fraction(1,2)}], "whoshouldpay": {10: None, 11: None, 12: Fraction(1,2)}}]
-    print(expenses)
-    print(solve_mincost_problem_for_expenses(expenses, [10,11,12]))
+    print(solve_mincost_problem_for_expenses([{"whopaid": [{"personId": 10, "amount": Fraction(1,2)}, {"personId": 11, "amount": Fraction(1,2)}], "whoshouldpay": {10: None, 11: None, 12: Fraction(1,2)}}] , [10,11,12], 1))
+    # invalid problems (invalid constraint because all people contribute an amount which is unequal the paid amount:
+    # from buddyledger.baconsvin.org/ledger/17
+    print(solve_mincost_problem_for_expenses([{'whopaid': [{'personId': 60, 'amount': Fraction(1,2)}], 'whoshouldpay': {60: Fraction(1, 6), 61: None}}], [61, 60], 1))
+    #                                                                                                                added to make feasible ^^^^^^^^
+    print(solve_mincost_problem_for_expenses([{'whopaid': [{'personId': 60, 'amount': Fraction(1,2)}], 'whoshouldpay': {60: Fraction(1, 6)}}], [61, 60], 1)) # would make an error
+    print(solve_mincost_problem_for_expenses([{'whopaid': [{'amount': Fraction(75, 1), 'personId': 60}], 'whoshouldpay': {60: Fraction(25, 1), 61: Fraction(25, 1)}}], [60,61], 1))
+    print(solve_mincost_problem_for_expenses([{'whopaid': [{'amount': Fraction(3, 1), 'personId': 60}], 'whoshouldpay': {60: Fraction(1, 1), 61: Fraction(1, 1)}}], [61, 60], 1))
